5e039d81ba12faf04be2c18331fa67aa727ae47d
Rasmus Reichardt <rasmrei@greeny.cs.tlu.ee>
2015-09-07 15:27:48 +0300

    login form

--- user_form.php
+++ user_form.php
@@ -1,7 +1,23 @@
 <?php
-
 	
+	//jutumärkide vahele input elemendi name
+	echo $_POST["email"];
+	echo $_POST["password"];
 
+	$email_error = "";
+	
+	//kontrolli ainult siis kui kasutaja vajutab login nuppu
+	if($_SERVER["REQUEST_METHOD"] == "POST") 
+	
+	//kontrollime kasutaja e-posti, et see ei oleks tühi
+	if(empty($_POST["email"])) {
+		echo "email tühi";
+			$email_error = "Ei saa olla tühi";
+		
+	//kontrolli parooli
+	
+	
+	}
 ?>
 <html>
 	<head>
@@ -9,10 +25,13 @@
 	<head/>
 	<body>
 		<h2>Login</h2>
-		<input type="email" placeholder="E-post"> <br><br>
-		<input type="password" placeholder="Parool">
+		<form action="user_form.php" method="post">
+		<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?> <br><br>
+		<input name="password" type="password" placeholder="Parool">
 		
 		<input type ="submit" value="logi sisse"; <br><br>
+		</form>
+		
 		<h2>Create user</h2>
 		
 		
