575537529958e117d69bdced3a8a48c1b9534636
Rasmus Reichardt <rasmrei@greeny.cs.tlu.ee>
2015-09-07 14:58:49 +0300

    login form

---
+++ user_form.php
@@ -0,0 +1,21 @@
+<?php
+
+	
+
+?>
+<html>
+	<head>
+		<title>User login page</title>
+	<head/>
+	<body>
+		<h2>Login</h2>
+		<input type="email" placeholder="E-post"> <br><br>
+		<input type="password" placeholder="Parool">
+		
+		<input type ="submit" value="logi sisse"; <br><br>
+		<h2>Create user</h2>
+		
+		
+	</body>
+
+</html>
