3f1bd384df2b1252cd8494087fd1a7fa16e5a8d5
Rasmus Reichardt <rasmrei@greeny.cs.tlu.ee>
2015-09-07 13:47:00 +0300

    added logic

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,25 @@
 <?php
 
-$first_name="Rasmus";
+	$first_name="Rasmus";
+	$last_name="Reichardt";
 
-//trükib välja 
-echo $first_name;
+	//trükib välja 
+	echo $first_name." ".$last_name;
+
+?>
+
+<br>
+
+<?php
+
+	$age=21;
+	
+	// if( loogikatehe ) { tõene} else { väär }]
+	if($age<18) {
+		echo "alaealine";
+	} else {
+		echo "täisealine";
+	}
+	
 
 ?>
